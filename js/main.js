02ba35ac4eac434dada9df9c4a925d3c9edbd2bc
Pablo Olmos <82683589+PualMark@users.noreply.github.com>
2021-11-09 15:55:59 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
-//Start Page on #home
-var url = document.URL,
-	index = url.indexOf("#"),
-	hash = index != -1 ? url.substring(index+1) : "";
+// Start on #home
+    var url = document.URL, 
+      index = url.indexOf("#"),
+      hash = index != -1 ? url.substring(index+1) : "";
 
-	if(hash === ""){
-		location.hash = "#home";
+    if(hash === ""){
+        location.hash = "#home";
     }
-
-//
-var homeId = document.getElementById('home-nav')
-var servicesId = document.getElementById('services-nav')
-var portfolioId = document.getElementById('portfolio-nav')
-var contactId = document.getElementById('contact-nav')
-
-	if(hash === "#home"){
-		homeId.style.backgroundColor = 'rgb(255, 255, 255, 0.1)'
-	}
 
 //Smooth Scroll
 $(document).ready(function(){
@@ -32,7 +22,7 @@ $(document).ready(function(){
 
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 400, function(){
+      }, 300, function(){
 
         window.location.hash = hash;
       });
@@ -40,45 +30,43 @@ $(document).ready(function(){
   });
 });
 
-// Toggle Button Function to Show Navigation Bar Mobile
-function showNavbar() {
-	//input
-	var checkBox = document.getElementById('show-nav')
-	//navigation bar
-	var content = document.getElementById('navbar')
-	//Toggle Lines
-	//line 1
-	var line1 = document.getElementById('toggle-line1')
-	//line 2
-	var line2 = document.getElementById('toggle-line2')
+//Email
+window.addEventListener("DOMContentLoaded", function () {
 
-	if(checkBox.checked == true){
-		content.style.display = 'block'
-		content.style.animation = 'nav-display .4s'
-		//lines
-		line1.style.animation = 'toggle-line-1 .4s forwards'
-		line2.style.animation = 'toggle-line-2 .4s forwards'
-	}else{
-		content.style.animation = 'nav-nodisplay .4s forwards'
-		line1.style.animation = 'toggle-line-out-1 .4s forwards'
-		line2.style.animation = 'toggle-line-out-2 .4s forwards'
-	}
-}
+  var form = document.getElementById("my-form");
+  var status = document.getElementById("status");
 
-// Services Slider
-var slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("s-slide");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks! :)";
   }
-  slideIndex++;
-  if (slideIndex > slides.length){
-  	slideIndex = 1
-	}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2500);
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "We are full! :(";
+  }
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
 }
