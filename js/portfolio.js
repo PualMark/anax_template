ed6f5fa91561c543526deb2fcb70bbc46b7719b6
Pablo Olmos <82683589+PualMark@users.noreply.github.com>
2021-11-09 09:50:46 -0600

    Add files via upload

---
+++ js/portfolio.js
@@ -0,0 +1,299 @@
+const projectData = [
+	{
+		image:'./img/projects/p01.jpg',
+		title:'Rendering Conceptual Museo',
+		text:'N.L M.L'
+	},
+	{
+		image:'./img/projects/p02.jpg',
+		title:'7 Eleven Monterrey',
+		text:'Nueva Imagen a Tienda Ecológica Musas con Certificación LEED'
+	},
+	{
+		image:'./img/projects/p03.jpg',
+		title:'Tim Hortons, Fashion Drive',
+		text:'Diseño primera tienda latinoaméricana con colaboración con el Arq. Emmanuel Hinojosa y Arq. Tania González'
+	},
+	{
+		image:'./img/projects/p04.jpg',
+		title:'Tim Hortons, Pabellón Monterrey',
+		text:'Diseño de oficinas administrativas'
+	},
+	{
+		image:'./img/projects/p05.jpg',
+		title:'Tim Hortons, Pabellón Monterrey',
+		text:'Diseño de oficinas administrativas'
+	},
+	{
+		image:'./img/projects/p06.jpg',
+		title:'Bites, Monterrey',
+		text:'Diseño y construcción de Restaurant'
+	},
+	{
+		image:'./img/projects/p07.jpg',
+		title:'Plaza LUA Monterrey',
+		text:'Diseño de Clínica'
+	},
+	{
+		image:'./img/projects/p08.jpg',
+		title:'R.E',
+		text:'Diseño y construcción de Residencia'
+	},
+	{
+		image:'./img/projects/p09.jpg',
+		title:'T.A Nuevo Laredo',
+		text:'Diseño y construcción de oficinas administrativas'
+	},
+	{
+		image:'./img/projects/p10.jpg',
+		title:'J.P.',
+		text:'Diseño de Residencia'
+	},
+	{
+		image:'./img/projects/p11.jpg',
+		title:'J.S.',
+		text:'Diseño de Quinta'
+	},
+	{
+		image:'./img/projects/p12.jpg',
+		title:'Rendering APPSA',
+		text:'Rendering'
+	},
+	{
+		image:'./img/projects/p13.jpg',
+		title:'J.P.',
+		text:'Diseño de Residencia'
+	},
+	{
+		image:'./img/projects/p14.jpg',
+		title:'Facultad de Biología UANL',
+		text:'Diseño de Área de Cómputo'
+	},
+	{
+		image:'./img/projects/p15.jpg',
+		title:'J.S',
+		text:'Diseño de Quinta'
+	},
+	{
+		image:'./img/projects/p16.jpg',
+		title:'J.S',
+		text:'Diseño de Quinta'
+	},
+	{
+		image:'./img/projects/p17.jpg',
+		title:'R.R.',
+		text:'Diseño de Interior'
+	},
+	{
+		image:'./img/projects/p18.jpg',
+		title:'APPSA',
+		text:'Diseño Comercial'
+	},
+	{
+		image:'./img/projects/p19.jpg',
+		title:'APPSA',
+		text:'Diseño Comercial'
+	},
+	{
+		image:'./img/projects/p20.jpg',
+		title:'C.G.',
+		text:'Diseño Comercial'
+	},
+	{
+		image:'./img/projects/p21.jpg',
+		title:'Carolco',
+		text:'Rendering Residencial'
+	},
+	{
+		image:'./img/projects/p22.jpg',
+		title:'Diseño Inteligente',
+		text:'Rendering Mobiliario'
+	},
+	{
+		image:'./img/projects/p23.jpg',
+		title:'M.S.',
+		text:'Diseño de Residencia de Descanso'
+	},
+	{
+		image:'./img/projects/p24.jpg',
+		title:'D.L.',
+		text:'Diseño de Quinta de Descanso'
+	},
+	{
+		image:'./img/projects/p25.jpg',
+		title:'INTERVIT',
+		text:'Diseño de Interiores de Oficinas'
+	},
+	{
+		image:'./img/projects/p26.jpg',
+		title:'INOVA',
+		text:'Rendering Residencial'
+	},
+	{
+		image:'./img/projects/p27.jpg',
+		title:'Hospital San José Tec de Monterrey',
+		text:'Construcción de Quirófano'
+	},
+	{
+		image:'./img/projects/p28.jpg',
+		title:'7-Eleven',
+		text:'Diseño de Nueva Imagen'
+	},
+	{
+		image:'./img/projects/p29.jpg',
+		title:'7-Eleven',
+		text:'Diseño de Nueva Imagen'
+	},
+	{
+		image:'./img/projects/p30.jpg',
+		title:'7-Eleven',
+		text:'Diseño de Nueva Imagen'
+	},
+	{
+		image:'./img/projects/p31.jpg',
+		title:'7-Eleven Certificación LEED 2ndo Lugar Premio Obras CEMEX 2014',
+		text:'Diseño de Tienda Udem'
+	},
+	{
+		image:'./img/projects/p33.jpg',
+		title:'OAT ICONN',
+		text:'Diseño y Fabricación de Mural de Comunicación'
+	},
+	{
+		image:'./img/projects/p34.jpg',
+		title:'OAT ICONN',
+		text:'Diseño y Fabricación de Mural de Comunicación'
+	},
+	{
+		image:'./img/projects/p35.jpg',
+		title:'FORMA FERRARI MASERATTI',
+		text:'Diseño y Fabricación de Stand de Exhibición'
+	},
+	{
+		image:'./img/projects/p36.jpg',
+		title:'FORMA FERRARI MASERATTI',
+		text:'Diseño y Fabricación de Stand de Exhibición'
+	},
+	{
+		image:'./img/projects/p37.jpg',
+		title:'7-Eleven Torreón, Coah',
+		text:'Diseño y Construcción de Oficinas 7-Eleven'
+	},
+	{
+		image:'./img/projects/p38.jpg',
+		title:'7-Eleven Torreón, Coah',
+		text:'Diseño y Construcción de Oficinas 7-Eleven'
+	},
+	{
+		image:'./img/projects/p39.jpg',
+		title:'7-Eleven Torreón, Coah',
+		text:'Diseño y Construcción de Oficinas 7-Eleven'
+	},
+	{
+		image:'./img/projects/p40.jpg',
+		title:'S.A Hermosillo, Son',
+		text:'Diseño y Construcción de Clínica Dental'
+	},
+	{
+		image:'./img/projects/p41.jpg',
+		title:'S.A Hermosillo, Son',
+		text:'Diseño y Construcción de Clínica Dental'
+	},
+	{
+		image:'./img/projects/p42.jpg',
+		title:'Torre de Rectoría UANL',
+		text:'Diseño de Remodelación de Fachadas'
+	},
+	{
+		image:'./img/projects/p43.jpg',
+		title:'Torre de Rectoría UANL',
+		text:'Diseño de Remodelación de Fachadas'
+	},
+	{
+		image:'./img/projects/p44.jpg',
+		title:'Red Estatal de Autopistas(REA) Gobierno de Nuevo León',
+		text:'Recorrido Virtual Rendering'
+	},
+	{
+		image:'./img/projects/p45.jpg',
+		title:'Plaza Duendes, San Pedro',
+		text:'Diseño y Construcción de Restaurant'
+	},
+	{
+		image:'./img/projects/p46.jpg',
+		title:'Plaza Duendes, San Pedro',
+		text:'Diseño y Construcción de Restaurant'
+	},
+	{
+		image:'./img/projects/p47.jpg',
+		title:'J.V. Monterrey',
+		text:'Construcción Área de Radiología Clínica Dental'
+	},
+	{
+		image:'./img/projects/p48.jpg',
+		title:'INOVA',
+		text:'Rendering Biblioteca'
+	},
+	{
+		image:'./img/projects/p49.jpg',
+		title:'Municipio de San Pedro Garza García',
+		text:'Rendering Tranvía'
+	},
+	{
+		image:'./img/projects/p50.jpg',
+		title:'7-Eleven',
+		text:'Diseño de Nueva Imagen y Adapatación a más de 250 tiendas'
+	},
+	{
+		image:'./img/projects/p51.jpg',
+		title:'Gobierno de Coahuila',
+		text:'Rendering Concurso'
+	},
+	{
+		image:'./img/projects/p52.jpg',
+		title:'Oficinas GreenRoom Monterrey',
+		text:'Diseño y Construcción de Oficinas GreenRoom'
+	},
+	{
+		image:'./img/projects/p53.jpg',
+		title:'Gobierno de Coahuila',
+		text:'Rendering Concurso'
+	},
+	{
+		image:'./img/projects/p54.jpg',
+		title:'I.G. Monterrey',
+		text:'Diseño y Construcción de Oficinas'
+	},
+	{
+		image:'./img/projects/p55.jpg',
+		title:'ROCA',
+		text:'Diseño y Construcción de Oficinas'
+	},
+	{
+		image:'./img/projects/p56.jpg',
+		title:'Cocina Monterrey',
+		text:'Diseño y Construcción de Cocina'
+	},
+	{
+		image:'./img/projects/p57.jpg',
+		title:'G.G.',
+		text:'Diseño y Construcción de Clínica Dental'
+	},
+	{
+		image:'./img/projects/p58.jpg',
+		title:'Apodaca NL',
+		text:'Diseño Industrial'
+	},
+];
+
+document.getElementById('project-card').innerHTML = `
+${projectData.map(function(project){
+	return `
+	<div class='proj'>
+		<p class='project-title'>${project.title}</p>
+		<img class='project-img' src="${project.image}">
+		<p class='project-text'>${project.text}</p>
+	</div>
+	`
+}).join('')}
+`
